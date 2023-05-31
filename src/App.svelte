e730c1d624bc376969df3ee081f6353ae15e8576
BuildTools <cephas8080@outlook.com>
2023-05-31 21:39:52 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1>Mots</h1>
+	<h2>revision / todo / timer all in one</h2>
+	<span>Coming "soon" on Windows and Linux</span>
+</main>
+
+<style>
+</style>
